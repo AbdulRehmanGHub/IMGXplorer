dce89f93beca764425c47efc2555ab10dda56d49
abdulrehmanghub <pakkhan9009@gmail.com>
2024-10-04 22:44:31 +0500

    image API JS

--- script.js
+++ script.js
@@ -2,43 +2,50 @@
 // URL:         https://api.unsplash.com/search/photos?page=1&query=office
 
 let page = 1;
-let accessKey = 'GZ6KFar1nVj8QBLdCSy33Bj8CsWn6JIA9Mu5sNBR2e4';
+let accessKey = "GZ6KFar1nVj8QBLdCSy33Bj8CsWn6JIA9Mu5sNBR2e4";
 
-let inputField = document.querySelector('#inp');
-let searchBtn = document.querySelector('#searchBtn');
-let imgg = document.querySelector('.imgs');
+let inputField = document.querySelector("#inp");
+let searchBtn = document.querySelector("#searchBtn");
+let imgg = document.querySelector(".imgs");
 
 let getAPI = async (value) => {
-    let url = `https://api.unsplash.com/search/photos?page=${page}&query=${value}&client_id=${accessKey}`;
-    let response = await fetch(url);
-    let data = await response.json();
-    let array = data.results;
+  let url = `https://api.unsplash.com/search/photos?page=${page}&query=${value}&client_id=${accessKey}`;
+  let response = await fetch(url);
+  let data = await response.json();
+  let array = data.results;
 
-    imgg.innerHTML = '';
+  imgg.innerHTML = "";
 
-    array.map((oneValue) => {
-        let newImg = document.createElement('div');
-        newImg.className = 'imageContainer2';
+  array.map((oneValue) => {
+    let newImg = document.createElement("div");
+    newImg.className = "imageContainer2";
 
-        let img = document.createElement('img');
-        img.src = oneValue.urls.small;
-        img.className = 'imgInContainer';
+    let img = document.createElement("img");
+    img.src = oneValue.urls.small;
+    img.className = "imgInContainer";
 
-        let imgDesc = document.createElement('p');
-        imgDesc.innerHTML = oneValue.alt_description;
-        imgDesc.id = 'img-description';
+    let imgDesc = document.createElement("p");
+    imgDesc.innerHTML = oneValue.alt_description;
+    imgDesc.id = "img-description";
 
-        imgg.append(newImg);
-        newImg.appendChild(img);
-        newImg.append(imgDesc);
-    });
+    imgg.append(newImg);
+    newImg.appendChild(img);
+    newImg.append(imgDesc);
+  });
 };
 
-searchBtn.addEventListener('click', () => {
-    if (inputField.value == '') {
-        alert('Please write something to search!');
-    } else {
-        let value = inputField.value;
-        getAPI(value);
-    }
+searchBtn.addEventListener("click", () => {
+  let inpValue = inputField.value;
+  if (inpValue == "") {
+    alert("Please write something to search!");
+  } else {
+    getAPI(inpValue);
+  }
+});
+
+inputField.addEventListener("keypress", (e) => {
+  let inpValue = inputField.value;
+  if (e.key === "Enter") {
+    getAPI(inpValue);
+  }
 });
